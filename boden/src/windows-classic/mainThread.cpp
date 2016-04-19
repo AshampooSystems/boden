1da1bd6eee3ff424d23822d127ea072bfba5f285
Hauke Duden <hdu@ashampoo.com>
2016-04-19 17:01:58 +0200

    first implementation of mainThread stuff

---
+++ boden/src/windows-classic/mainThread.cpp
@@ -0,0 +1,105 @@
+#include <bdn/init.h>
+#include <bdn/mainThread.h>
+
+
+#include <windows.h>
+
+namespace bdn
+{
+
+
+class GlobalMessageWindow
+{
+public:
+	GlobalMessageWindow()
+	{
+		WNDCLASSEX cls;
+		memset(&cls, 0, sizeof(cls));
+
+		cls.cbSize = sizeof(WNDCLASSEX);
+		cls.lpfnWndProc = windowProc;
+		cls.lpszClassName = L"bdn::globalMessageWindow";
+
+		::RegisterClass(&cls);
+
+		_windowHandle = ::CreateWindowEx(
+			0,
+			cls.lpszClassName,
+			L"bdn.globalMessageWindow",
+			WS_POPUP,
+			0,
+			0,
+			0,
+			0,
+			NULL,
+			NULL,
+			::GetModuleInstance(),
+			this );
+
+		if(_windowHandle==NULL)
+			throwLastSysError();
+	}
+
+
+	HWND getHandle()
+	{
+		return _windowHandle;
+	}
+
+	void postCallMessage(ICallable* pCallable)
+	{
+		pCallable->addRef();
+		::PostMessage(_windowHandle, CallMessage, 0, (LPARAM)pCallable );
+	}
+
+	static P<GlobalMessageWindow> get()
+	{
+		static SafeInit<GlobalMessageWindow> init;
+
+		init.get();
+	}
+
+protected:
+	
+	enum
+	{
+		CallMessage = WM_USER+100
+	};
+
+
+	static LRESULT windowProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		if(message==WM_CREATE)
+		{
+			CREATESTRUCT* pInfo = (CREATESTRUCT*)lParam;
+			
+			::SetWindowLongPtr(_windowHandle, (LONG_PTR)pInfo->lpCreateParams);
+		}
+
+		if(message==CallMessage)
+		{
+			ICallable* pCallable = (ICallable*)lParam;
+
+			pCallable->call();
+
+			pCallable->release();
+
+			return 0;
+		}
+
+		return CallWindowProc(DefWindowProc, windowHandle, message, wParam, lParam);
+	}
+
+
+	HWND _windowHandle;
+};
+
+
+void CallFromMainThread_::dispatch()
+{
+	GlobalMessageWindow::get()->postCallMessage(this);
+}
+
+
+}
+
