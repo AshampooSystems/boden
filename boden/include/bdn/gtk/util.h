24a75468653c61c7e80ae4863752f5a299ad9e2d
Hauke Duden <hdu@ashampoo.com>
2016-07-01 18:38:49 +0200

    gtk fixes

---
+++ boden/include/bdn/gtk/util.h
@@ -0,0 +1,60 @@
+#ifndef BDN_GTK_util_H_
+#define BDN_GTK_util_H_
+
+#include <gtk/gtk.h>
+
+namespace bdn
+{
+namespace gtk
+{
+
+int gtkDimensionToPixels(int dim, double scaleFactor)
+{
+    return std::lround(dim*scaleFactor);
+}
+
+int pixelsToGtkDimension(int pixels, double scaleFactor)
+{
+    int dim = pixels / scaleFactor;
+    if(dim==0 && pixels>0)
+        dim = 1;
+        
+    return dim;
+}
+
+Rect gtkRectToRect(const GdkRectangle& rect, double scaleFactor )
+{
+	return Rect(
+			std::lround(rect.x * scaleFactor),
+			std::lround(rect.x * scaleFactor),
+			gtkDimensionToPixels(rect.width, scaleFactor),
+			gtkDimensionToPixels(rect.height, scaleFactor) );
+}
+
+GdkRectangle rectToGtkRect(const Rect& rect, double scaleFactor)
+{
+    GdkRectangle result;
+    
+    result.x = std::lround( rect.x / scaleFactor);
+    result.y = std::lround( rect.y / scaleFactor);
+    result.width = pixelsToGtkDimension(rect.width, scaleFactor);
+    result.height = pixelsToGtkDimension(rect.height, scaleFactor);
+    
+    return result;
+}
+
+
+Size gtkSizeToSize(const GtkRequisition& size, double scaleFactor)
+{
+    return Size( gtkDimensionToPixels(size.width, scaleFactor),
+                 gtkDimensionToPixels(size.height, scaleFactor) );
+}
+
+		
+}
+}
+
+
+#endif
+
+
