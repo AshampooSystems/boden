eff049cf48e99ccb034573cf7645993d482c4be4
Hauke Duden <hdu@ashampoo.com>
2017-08-16 14:52:53 +0200

    improved isUtf8Locale to use encoding instead of decoding

--- boden/src/localeUtil.cpp
+++ boden/src/localeUtil.cpp
@@ -23,79 +23,68 @@ std::locale deriveUtf8Locale(const std::locale& baseLocale)
 
 bool isUtf8Locale(const std::locale& loc)
 {
-    if( std::has_facet< CodecVtChar16Char >(loc) )
-    {
-        const CodecVtChar16Char& codec = std::use_facet< CodecVtChar16Char >(loc);
-
-        if( typeid(codec) == typeid(CodecVtUtf8Utf16) )
-            return true;
-
-        // some systems might also use the UTF-8 to UCS-2 codec here (instead of UTF-8 to UTF-16)
-        if( typeid(codec) == typeid(CodecVtUtf8Ucs2) )
-            return true;
-    }
-
-    if( std::has_facet< CodecVtChar32Char >(loc) )
-    {
-        const CodecVtChar32Char& codec = std::use_facet< CodecVtChar32Char >(loc);
-
-        if( typeid(codec) == typeid(CodecVtUtf8Utf32) )
-            return true;
-
-        // normally UCS4 is the same as UTF-32, but we check anyway
-        if( typeid(codec) == typeid(CodecVtUtf8Ucs4) )
-            return true;
-    }
-
     if( std::has_facet< std::codecvt<wchar_t,char,mbstate_t> >(loc) )
     {
         const std::codecvt<wchar_t,char,mbstate_t>& codec = std::use_facet< std::codecvt<wchar_t,char,mbstate_t> >(loc);
 
-        if( typeid(codec) == typeid(std::codecvt_utf8<wchar_t>) )
+        if( dynamic_cast< const std::codecvt_utf8<wchar_t>* >(&codec) != nullptr
+            || dynamic_cast< const std::codecvt_utf8_utf16<wchar_t>* >(&codec) != nullptr)
+        {
+            // the codec is derived from std::codecvt_utf8 or std::codecvt_utf8_utf16.
+            // So we know that it is a UTF-8 codec.
             return true;
+        }
     
-        // we do not recognize any of the codec types. But it might still be a custom codec that
-        // implements UTF-8. We check for that by trying to decode a UTF-8 sequence.
-        // Note that this can be a little risky: some codecs have bugs that lead to a crash when
-        // the data is not encoded correctly.
-        // To reduce the likelihood for such a bug to trigger we make sure that the sequence
-        // we decode is embedded in a "sea of zeros", to make sure that any misguided attempts
-        // to read data outside the actual sequence do not cause a crash.
+        // we do not recognize the codec type. But it might still be a custom codec that
+        // implements UTF-8. We check for that by encoding a character sequence and
+        // checking if the result is UTF-8.
+        // Note that this should not cause any problems if it is the wrong codec. Since we encode,
+        // rather than decode, it is unlikely that a codec bug would trigger a crash. The only
+        // thing that can happen is if the specified characters cannot represented with the
+        // particular multibyte codec (if it is not UTF-8). But that is a common case and
+        // we can assume that all production level codecs handle that case properly.
 
-        // We encode a character that fits into a 16 bit value and does not need a surrogate pair when
-        // encoded as UTF-16. We also make sure that the character has a good mix of 1 bits and 0 bits
-        // to make it less likely that some other encoding gives us the same result.
-
-        // We use a three-byte sequence. Some utf-8 implementations have problems with longer sequences.
-        const char* pUtf8WithZeros = u8"\0\0\0\0\0\0\0\0\uea7d\0\0\0\0\0\0\0\0";
-
-        const char* pUtf8Begin = pUtf8WithZeros+8;    
-        const char* pUtf8End = pUtf8Begin+3;
-        const char* pUtf8Next = pUtf8Begin;
-
-        wchar_t outBuffer[16] = {0};
-        wchar_t* pOutNext = outBuffer;
+        // Note that \u0197 produces a two byte UTF-8 sequence and \uea7d produces a three byte UTF-8 sequence.
+        // We also add a pure ascii character. None of these characters fall into the surrogate pair range for
+        // UTF-16, so it does not matter whether the particular codec treats wchar_t strings as UTF-16, UCS-2 or
+        // UTF-32. And if the encoded data matches the UTF-8 data for all three characters
+        // then we can be reasonably sure that the codec is indeed UTF-8 and that it is not a coincidence that
+        // the encoded sequences are the same.
+        const wchar_t* pInBegin = L"g\u0197\uea7d";
+        const char*    pExpectedUtf8 = u8"g\u0197\uea7d";
+        const wchar_t* pInEnd = pInBegin + wcslen(pInBegin);
+        const wchar_t* pInNext = nullptr;
+        
+        // the UTF-8 data would be 6 bytes long.
+        // But we provide a much larger buffer to ensure that the encoded data
+        // can fit for all codecs. If the data does not fit then that might trigger a bug in the codec
+        // that leads to a crash.
+        char outBuffer[3*8+1] = {0};
+        char* pOutEnd = outBuffer + sizeof(outBuffer) -1;
+        char* pOutNext = outBuffer;
 
         std::mbstate_t state = std::mbstate_t();
 
-        std::codecvt_base::result result = codec.in(
+        std::codecvt_base::result result = codec.out(
             state,
-            pUtf8Begin,
-            pUtf8End,
-            pUtf8Next,
+            pInBegin,
+            pInEnd,
+            pInNext,
             outBuffer,
-            outBuffer+16,
+            pOutEnd,
             pOutNext
             );
 
         if(result==std::codecvt_base::ok)
         {
-            // the codec says that it was able to decode this.
+            // the codec says that it was able to encode the data.
             // Verify that the result is what we expected.
-
-            if(pOutNext == &outBuffer[1] && outBuffer[0]==0xea7d )
+            int expectedLength = strlen(pExpectedUtf8);
+            
+            if(pOutNext == outBuffer+expectedLength
+                && memcmp(outBuffer, pExpectedUtf8, expectedLength)==0)
             {
-                // it seems that we have utf-8
+                // the encoded data is UTF-8
                 return true;
             }
         }
--- testboden/src/testLocaleUtil.cpp
+++ testboden/src/testLocaleUtil.cpp
@@ -5,6 +5,81 @@
 
 using namespace bdn;
 
+
+/** Wraps another codec to hide its type (used for testing).*/
+template<class InnerCodecType>
+class CodecVtWrapper_ : public std::codecvt<typename InnerCodecType::intern_type, typename InnerCodecType::extern_type, typename InnerCodecType::state_type>
+{
+public:
+
+private:
+
+    typedef typename InnerCodecType::intern_type MyInternType;
+    typedef typename InnerCodecType::extern_type MyExternType;
+    typedef typename InnerCodecType::state_type MyStateType;
+
+protected:
+
+    result do_out( MyStateType& state,
+            const MyInternType* from,
+            const MyInternType* from_end,
+            const MyInternType*& from_next,
+            MyExternType* to,
+            MyExternType* to_end,
+            MyExternType*& to_next ) const override
+    {
+        return _innerCodec.out(state, from, from_end, from_next, to, to_end, to_next);
+    }
+
+    result do_in( MyStateType& state,
+            const MyExternType* from,
+            const MyExternType* from_end,
+            const MyExternType*& from_next,
+            MyInternType* to,
+            MyInternType* to_end,
+            MyInternType*& to_next ) const override
+    {
+        return _innerCodec.in(state, from, from_end, from_next, to, to_end, to_next);
+    }
+
+    result do_unshift( MyStateType& state,
+                           MyExternType* to,
+                           MyExternType* to_end,
+                           MyExternType*& to_next) const override
+    {
+        return _innerCodec.unshift(state, to, to_end, to_next);
+    }
+        
+    int do_encoding() const noexcept override
+    {
+        return _innerCodec.encoding();
+    }
+
+    bool do_always_noconv() const noexcept override
+    {
+        return _innerCodec.always_noconv();
+    }
+
+    int do_length( MyStateType& state,
+                       const MyExternType* from,
+                       const MyExternType* from_end,
+                       std::size_t max ) const override
+    {
+        return _innerCodec.length(state, from, from_end, max);
+    }
+    
+    int do_max_length() const noexcept override
+    {
+        return _innerCodec.max_length();
+    }
+        
+
+private:
+    
+    InnerCodecType _innerCodec;
+};
+
+
 TEST_CASE("localeUtil")
 {
     SECTION("CodecVt typedefs")
@@ -80,8 +155,20 @@ TEST_CASE("localeUtil")
 
         // verify that the locale uses utf-8 codecs
         REQUIRE( typeid( std::use_facet< std::codecvt<wchar_t,char,mbstate_t> >(loc) ) == typeid( std::codecvt_utf8<wchar_t> ) );
-        REQUIRE( typeid( std::use_facet< CodecVtChar16Char >(loc) ) == typeid( CodecVtUtf8Utf16 ) );
-        REQUIRE( typeid( std::use_facet< CodecVtChar32Char >(loc) ) == typeid( CodecVtUtf8Utf32 ) );
+
+        
+#if defined(_MSC_VER) && _MSC_VER>=1900 && _MSC_VER<=1901  // Visual Studio 2015 and 2017
+        // the necessary specializations for std::use_facet for char16_t and char32_t are missing
+        // in Visual Studio 2015 and 2017 (even though these are standard facets defined by the standard).
+        // Note that there is also no use_facet specialization for the alternative codecvt implementations
+        // for unsigned int and unsigned short.
+        // So we cannot test this if we have these compiler versions.
+
+        // So, do nothing here.
+#else
+        REQUIRE( typeid( std::use_facet< std::codecvt<char16_t,char,mbstate_t> >(loc) ) == typeid( CodecVtUtf8Utf16 ) );
+        REQUIRE( typeid( std::use_facet< std::codecvt<char32_t,char,mbstate_t> >(loc) ) == typeid( CodecVtUtf8Utf32 ) );
+#endif
 
         // verify that the other settings are still the same
         REQUIRE( typeid( std::use_facet< std::moneypunct<char> >(loc) ) == typeid( std::use_facet< std::moneypunct<char> >(origLoc) )  );
@@ -119,7 +206,7 @@ TEST_CASE("localeUtil")
             REQUIRE( isUtf8Locale(loc) );
         }
 
-        SECTION("custom utf8")
+        SECTION("custom utf8 derived from codecvt_utf8")
         {
             // we derive our own types from the standard library ones and set them in the locale.
 
@@ -139,6 +226,17 @@ TEST_CASE("localeUtil")
             std::locale loc = std::locale ( std::locale::classic(), new MyUtf8Wchar );
             loc = std::locale( loc, new MyUtf8Utf16 );
             loc = std::locale( loc, new MyUtf8Utf32 );
+
+            REQUIRE( isUtf8Locale(loc) );
+        }
+
+        SECTION("custom utf8 not derived from codecvt_utf8")
+        {
+            // we derive our own types from the standard library ones and set them in the locale.
+                       
+            std::locale loc = std::locale ( std::locale::classic(), new CodecVtWrapper_< std::codecvt_utf8<wchar_t> > );
+            loc = std::locale( loc, new CodecVtWrapper_< CodecVtUtf8Utf16 > );
+            loc = std::locale( loc, new CodecVtWrapper_< CodecVtUtf8Utf32 > );
 
             REQUIRE( isUtf8Locale(loc) );
         }
