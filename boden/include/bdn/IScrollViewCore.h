a4b31f086c7cebb9651048e5b31a6037db33b1a8
Hauke Duden <hdu@ashampoo.com>
2017-08-24 17:51:29 +0200

    begun scrollToVisible

--- boden/include/bdn/IScrollViewCore.h
+++ boden/include/bdn/IScrollViewCore.h
@@ -18,6 +18,15 @@ public:
     /** Controls wether or not the view scrolls vertically.*/
 	virtual void setVerticalScrollingEnabled(const bool& enabled)=0;
     
+
+    /** Changes the scroll position so that the specified area is visible.
+        The specified rect coordinates refer to scroll view's inner scrolled
+        coordinate system. I.e. The point (0,0) refers to the top left corner
+        of the scrolled area, which is not necessarily the same
+        of the top-left corner the content view (if the content view has a nonzero margin,
+        or if the scroll view has nonzero padding).
+        */
+    virtual void scrollAreaToVisible(const Rect& area)=0;
 	
 };
 
