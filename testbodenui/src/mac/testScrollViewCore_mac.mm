8129d21518556b4ca6488e8d34546a903d0a2363
Hauke Duden <hdu@ashampoo.com>
2017-09-25 16:30:06 +0200

    adapted mac tests to latest changes

--- testbodenui/src/mac/testScrollViewCore_mac.mm
+++ testbodenui/src/mac/testScrollViewCore_mac.mm
@@ -61,17 +61,23 @@ protected:
         return bdn::Size(0,0);
     }
     
-    void initiateScrollViewResizeToHaveViewPortSize( const bdn::Size& viewPortSize) override
+    bdn::Size initiateScrollViewResizeToHaveViewPortSize( const bdn::Size& viewPortSize) override
     {
+        bdn::Size viewSize = viewPortSize + getNonClientSize();
+        
+        viewSize = _pScrollView->adjustBounds( bdn::Rect( _pScrollView->position(), viewSize), RoundType::nearest, RoundType::nearest ).getSize();
+        
         // we cannot resize the scroll view directly with adjustAndSetBounds.
         // That would not have any effect outside of a layout cycle.
         // Instead we set the preferred size min and max to force the outer view
         // to resize it to the specified size.
         
-        _pScrollView->preferredSizeMinimum() = viewPortSize+getNonClientSize();
-        _pScrollView->preferredSizeMaximum() = viewPortSize+getNonClientSize();
+        _pScrollView->preferredSizeMinimum() = viewSize;
+        _pScrollView->preferredSizeMaximum() = viewSize;
         
         _pWindow->requestAutoSize();
+        
+        return viewSize;
     }
     
     void verifyScrollsHorizontally( bool expectedScrolls) override
