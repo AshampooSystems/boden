9f9be7c1fb067cb01f4da98040f695eca14608db
Hauke Duden <hdu@ashampoo.com>
2018-03-27 11:25:54 +0200

    Fix for new synchronous notifiers

--- boden/include/bdn/gtk/ButtonCore.h
+++ boden/include/bdn/gtk/ButtonCore.h
@@ -47,7 +47,7 @@ public:
         {
             ClickEvent evt( pView );
 
-            cast<Button>( pView )->onClick().postNotification(evt);
+            cast<Button>( pView )->onClick().notify(evt);
         }
     }
 
