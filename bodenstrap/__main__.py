770d518a98c3a98124af1784477cb4717a4513bd
gitoby <35229100+gitoby@users.noreply.github.com>
2019-02-06 11:05:32 +0100

    Remove bodenstrap, remove outdated doxygen pages

--- bodenstrap/__main__.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-from . import main
-
-exit_code = main.do_main(sys.argv[1:])
-sys.exit( exit_code )
-
-
--- bodenstrap/plugins/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    "android",
-    "cmake",
-    "gradle",
-    "java"
-    ]
