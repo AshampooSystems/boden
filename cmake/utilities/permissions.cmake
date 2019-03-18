58435df5768e8a0e8c2e421790e487a1bb6e6b12
maddimax <mti@ashampoo-systems.com>
2019-03-18 09:02:04 +0100

    Adding Boden::All pseudo Library (#192)
    * Fixing Launchboard / AppIcon deployment for IOS
    * Consolidating demo CMake commands
    * Adding permissions.cmake helper macros
    * Adding environment variable "BODEN_TEAM_ID" for IOS Signing automation

---
+++ cmake/utilities/permissions.cmake
@@ -0,0 +1,29 @@
+macro(configure_app_permissions)
+    set(options ALLOW_INTERNET ALLOW_HTTP)
+
+    cmake_parse_arguments(_permissions "${options}" "" "" ${ARGN} )
+
+    if(BDN_PLATFORM_IOS OR BDN_PLATFORM_OSX)
+        set(_transport_security "<key>NSAppTransportSecurity</key><dict>")
+
+        # Allow HTTP
+        string(APPEND _transport_security "<key>NSAllowsArbitraryLoadsInWebContent</key>")
+
+        if(_permissions_ALLOW_HTTP)
+            string(APPEND _transport_security "<true/>")
+        else()
+            string(APPEND _transport_security "<false/>")
+        endif()
+
+        string(APPEND _transport_security "</dict>")
+        set(MACOSX_NSAPP_TRANSPORT_SECURITY ${_transport_security})
+    elseif(BDN_PLATFORM_ANDROID)
+        set(ANDROID_PERMISSIONS "")
+
+        if(_permissions_ALLOW_INTERNET)
+            string(APPEND ANDROID_PERMISSIONS "<uses-permission android:name=\"android.permission.INTERNET\" />")
+        endif()
+    endif()
+endmacro()
+
+
