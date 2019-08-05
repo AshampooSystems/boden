18137be72bfa84fb3ab7525bb4b2454ac231fedd
Tobias Lensing <35229100+gitoby@users.noreply.github.com>
2019-08-05 15:38:11 +0200

    Use static field instead of enum class for bdn::android::wrapper::EditorInfo (#341)

--- framework/ui/platforms/android/include/bdn/android/wrapper/EditorInfo.h
+++ framework/ui/platforms/android/include/bdn/android/wrapper/EditorInfo.h
@@ -1,26 +1,38 @@
 #pragma once
 
+#include <bdn/java/StaticField.h>
+#include <bdn/java/wrapper/Object.h>
+
 namespace bdn::android::wrapper
 {
-    enum class EditorInfo : int64_t
+    constexpr const char kEditorInfoClassName[] = "android/view/inputmethod/EditorInfo";
+
+    class EditorInfo : public java::wrapper::JTObject<kEditorInfoClassName>
     {
-        IME_MASK_ACTION = 0x000000ff,
-        IME_ACTION_UNSPECIFIED = 0x00000000,
-        IME_ACTION_NONE = 0x00000001,
-        IME_ACTION_GO = 0x00000002,
-        IME_ACTION_SEARCH = 0x00000003,
-        IME_ACTION_SEND = 0x00000004,
-        IME_ACTION_NEXT = 0x00000005,
-        IME_ACTION_DONE = 0x00000006,
-        IME_ACTION_PREVIOUS = 0x00000007,
-        IME_FLAG_NO_PERSONALIZED_LEARNING = 0x1000000,
-        IME_FLAG_NO_FULLSCREEN = 0x2000000,
-        IME_FLAG_NAVIGATE_PREVIOUS = 0x4000000,
-        IME_FLAG_NAVIGATE_NEXT = 0x8000000,
-        IME_FLAG_NO_EXTRACT_UI = 0x10000000,
-        IME_FLAG_NO_ACCESSORY_ACTION = 0x20000000,
-        IME_FLAG_NO_ENTER_ACTION = 0x40000000,
-        IME_FLAG_FORCE_ASCII = 0x80000000,
-        IME_NULL = 0x00000000
+      public:
+        using java::wrapper::JTObject<kEditorInfoClassName>::JTObject;
+
+      public:
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_MASK_ACTION{"IME_MASK_ACTION"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_UNSPECIFIED{"IME_ACTION_UNSPECIFIED"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_NONE{"IME_ACTION_NONE"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_GO{"IME_ACTION_GO"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_SEARCH{"IME_ACTION_SEARCH"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_SEND{"IME_ACTION_SEND"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_NEXT{"IME_ACTION_NEXT"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_DONE{"IME_ACTION_DONE"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_ACTION_PREVIOUS{"IME_ACTION_PREVIOUS"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NO_PERSONALIZED_LEARNING{
+            "IME_FLAG_NO_PERSONALIZED_LEARNING"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NO_FULLSCREEN{"IME_FLAG_NO_FULLSCREEN"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NAVIGATE_PREVIOUS{
+            "IME_FLAG_NAVIGATE_PREVIOUS"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NAVIGATE_NEXT{"IME_FLAG_NAVIGATE_NEXT"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NO_EXTRACT_UI{"IME_FLAG_NO_EXTRACT_UI"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NO_ACCESSORY_ACTION{
+            "IME_FLAG_NO_ACCESSORY_ACTION"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_NO_ENTER_ACTION{"IME_FLAG_NO_ENTER_ACTION"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_FLAG_FORCE_ASCII{"IME_FLAG_FORCE_ASCII"};
+        constexpr static java::StaticFinalField<int, EditorInfo> IME_NULL{"IME_NULL"};
     };
 }
